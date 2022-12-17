0f8b2506a4f2d72ace1992f042cdd42ddd8d4e39
Ivan <shilkin-ivan@mail.ru>
2022-12-17 15:03:30 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+for(int i=1; i <= B; i++)
+  {
+    n = n * A;
+  }
+Console.WriteLine(n);
