d4251b830a7d2a8c4df9a1fd52d6d7ae7c6e4ee1
Ivan <shilkin-ivan@mail.ru>
2022-12-17 15:34:48 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int l = Convert.ToString(n).Length;
+int a = 0;
+int b = 0;
+for (int i = 0; i < l; i++)
+{
+    a = n - n % 10;
+    b = b + (n - a);
+    n = n / 10;
+}
+Console.WriteLine(b);
